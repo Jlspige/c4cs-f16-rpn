53140796ff450970ec5b531e849a99418e3b8bd1
jlspige <j.spigelman@gmail.com>
2016-10-10 15:32:43 -0400

    added exponentiation test

--- test_rpn.py
+++ test_rpn.py
@@ -15,6 +15,9 @@ class TestBasics(unittest.TestCase):
 	def test_divide(self):
 		result = rpn.calculate("6 3 /")
 		self.assertEqual(2, result)
+	def test_exponentiation(self):
+		result = rpn.calculate("5 2 ^")
+		self.assertEqual(25, result)
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
